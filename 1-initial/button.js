07e6a2f85a395dcc7e5b4b83414d06ea8728d944
Antonio Ruberto <aruberto@users.noreply.github.com>
2016-03-21 11:57:26 -0400

    make thinner

--- 1-initial/button.js
+++ 1-initial/button.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import cx from 'classnames';
 
-const Button = ({ error, ...restProps }) =>
-  <button {...restProps} className={cx('button', { error })} />;
+const Button = ({ error, ...restProps }) => (
+  <button
+    {...restProps}
+    className={cx('button', { error })}
+  />
+);
 
 Button.propTypes = {
   error: React.PropTypes.bool,
